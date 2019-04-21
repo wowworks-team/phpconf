872082951758392c475e09f248250db5bc88d6b3
Victor Demin <vdemin@wowworks.ru>
2019-04-21 13:57:21 +0400

    add container example;

--- common/config/main.php
+++ common/config/main.php
@@ -5,6 +5,16 @@ return [
         'common\config\BootstrapContainer',
         'common\config\BootstrapEvents',
     ],
+    // https://www.yiiframework.com/doc/guide/2.0/en/concept-configurations#application-configurations
+    // https://www.yiiframework.com/doc/guide/2.0/en/concept-di-container#advanced-practical-usage
+    'container' => [
+        'definitions' => [
+            'yii\widgets\LinkPager' => ['maxButtonCount' => 5]
+        ],
+        'singletons' => [
+            // Dependency Injection Container singletons configuration
+        ]
+    ],
     'components' => [
         // ...
     ],
